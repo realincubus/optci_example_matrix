248eead5bedaafe88cd543e2fce9e18cc1c85010
skemnitz <kemnitz.stefan@googlemail.com>
2017-05-30 21:13:08 +0200

    initial

---
+++ main.cpp
@@ -0,0 +1,42 @@
+
+#include <numeric>
+#include <iostream>
+#include <memory>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+int main(int argc, char** argv){
+
+  auto size = stoi(argv[1]);
+
+  unsigned int X = size;
+  unsigned int Y = size;
+  unique_ptr<double[]> matrix (new double[Y*X]); 
+
+  unique_ptr<double[]> inputVector (new double[X]);
+  unique_ptr<double[]> targetVector (new double[Y]);
+
+  std::iota(&matrix[0],&matrix[Y*X],0);
+  std::iota(&inputVector[0],&inputVector[X],0);
+  std::fill(&targetVector[0], &targetVector[Y], 0); 
+
+  int iterations = 0;
+  auto start = std::chrono::high_resolution_clock::now();
+
+  for( iterations = 0; iterations < 100 ; iterations++ ) {
+    for ( int y = 0; y != Y; y++ ){
+      for ( int x = 0; x != X; x++ ) { 
+        targetVector[y] += matrix[x+y*X] * inputVector[x];
+      }   
+    }   
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+
+  auto ms = std::chrono::duration_cast<milliseconds>(end - start).count();
+  std::cout << "one mult takes " << ms / iterations << " ms val " << targetVector[0] << std::endl;
+
+  return 0;
+}
